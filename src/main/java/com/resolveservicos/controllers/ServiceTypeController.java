84fb1df53585a6777f9c35df61ec34663cdb1f46
Marcos1327 <giovannygigi@gmail.com>
2024-07-18 15:11:44 -0300

    create endpoint for service type and tested

--- src/main/java/com/resolveservicos/controllers/ServiceTypeController.java
+++ src/main/java/com/resolveservicos/controllers/ServiceTypeController.java
@@ -1,9 +1,46 @@
 package com.resolveservicos.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.resolveservicos.entities.dto.ServiceTypeRecord;
+import com.resolveservicos.entities.model.ServiceType;
+import com.resolveservicos.services.ServiceTypeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/service-types")
+@RequestMapping("/api/v1/service-types")
 public class ServiceTypeController {
+
+    private final ServiceTypeService serviceTypeService;
+
+    public ServiceTypeController(ServiceTypeService serviceTypeService) {
+        this.serviceTypeService = serviceTypeService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ServiceType> createServiceType(@RequestBody ServiceTypeRecord serviceTypeDTO) {
+        return ResponseEntity.ok(serviceTypeService.createServiceType(serviceTypeDTO));
+    }
+
+    @GetMapping("/find-all")
+    public ResponseEntity<List<ServiceType>> findAllServiceTypes() {
+        return ResponseEntity.ok(serviceTypeService.getAllServiceTypes());
+    }
+
+    @GetMapping("/find-by-id/{id}")
+    public ResponseEntity<ServiceType> findServiceTypeById(@PathVariable Long id) {
+        return ResponseEntity.ok(serviceTypeService.getServiceTypeById(id));
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<ServiceType> updateServiceType(@PathVariable Long id, @RequestBody ServiceTypeRecord serviceTypeDTO) {
+        return ResponseEntity.ok(serviceTypeService.updateServiceType(id, serviceTypeDTO));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteServiceType(@PathVariable Long id) {
+        serviceTypeService.deleteServiceType(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/resolveservicos/services/ServiceTypeService.java
+++ src/main/java/com/resolveservicos/services/ServiceTypeService.java
@@ -1,7 +1,65 @@
 package com.resolveservicos.services;
 
+import com.resolveservicos.entities.dto.ServiceTypeRecord;
+import com.resolveservicos.entities.model.ServiceType;
+import com.resolveservicos.handlers.ResourceNotFoundException;
+import com.resolveservicos.repositories.ServiceTypeRepository;
+import com.resolveservicos.utils.Util;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ServiceTypeService {
+
+    private final ServiceTypeRepository serviceTypeRepository;
+    private final Util util;
+
+    public ServiceTypeService(ServiceTypeRepository serviceTypeRepository, Util util) {
+        this.serviceTypeRepository = serviceTypeRepository;
+        this.util = util;
+    }
+
+    public ServiceType createServiceType(ServiceTypeRecord serviceTypeRecord) {
+        ServiceType serviceType = new ServiceType();
+
+        if ( util.isNullOrEmpty(serviceTypeRecord.serviceTypeName())) {
+            throw new ResourceNotFoundException("Service Type is required!");
+        }
+
+        serviceType.setServiceTypeName(serviceTypeRecord.serviceTypeName());
+        serviceType.setDescription(serviceTypeRecord.description());
+
+        serviceTypeRepository.save(serviceType);
+        return serviceType;
+    }
+
+    public List<ServiceType> getAllServiceTypes() {
+        return serviceTypeRepository.findAll();
+    }
+
+    public ServiceType getServiceTypeById(Long serviceTypeId) {
+        return findById(serviceTypeId);
+    }
+
+    public void deleteServiceType(Long serviceTypeId) {
+        ServiceType serviceType = findById(serviceTypeId);
+        serviceTypeRepository.delete(serviceType);
+    }
+    public ServiceType updateServiceType(Long serviceTypeId, ServiceTypeRecord serviceTypeRecord) {
+        ServiceType serviceType = findById(serviceTypeId);
+
+        if (serviceTypeRecord.serviceTypeName() != null) {
+            serviceType.setServiceTypeName(serviceTypeRecord.serviceTypeName());
+        }
+        if (serviceTypeRecord.description() != null) {
+            serviceType.setDescription(serviceTypeRecord.description());
+        }
+
+        serviceTypeRepository.save(serviceType);
+        return serviceType;
+    }
+    private ServiceType findById(Long serviceTypeId) {
+        return serviceTypeRepository.findById(serviceTypeId).orElseThrow(() -> new ResourceNotFoundException("Service Type not found with id: " + serviceTypeId));
+    }
 }
